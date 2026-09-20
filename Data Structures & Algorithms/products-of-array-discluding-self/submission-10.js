5f2971f8a107521b20f5446d75fd29e20de9c856
sidan <rosidanfaisal@gmail.com>
2026-09-20 15:40:39 +0700

    Add: products-of-array-discluding-self - submission-10

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-10.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let rightArr = []
+        let leftArr = []
+        let result = []
+        let runningLeft = 1
+        let runningRight = 1
+
+        // left and right loop
+        for(let i = 0; i < nums.length; i++){
+            let rIdx = nums.length - i - 1 // track from back of the array
+
+            // edge case [0], Left part
+            if(nums[i-1] === undefined){
+                leftArr[i] = runningLeft
+                runningLeft *= nums[i]
+            }else{
+                leftArr[i] = runningLeft
+                runningLeft *= nums[i]
+            }
+
+            // edge case [nums.length], right part
+            if(nums[i+1] === undefined){
+                rightArr[i] = runningRight
+                runningRight *= nums[rIdx]
+            }else{
+                rightArr[i] = runningRight
+                runningRight *= nums[rIdx]
+            }
+
+        }
+
+        for(let j = 0; j < nums.length; j++){
+            let rIdx = nums.length - j - 1
+            let res = leftArr[j] * rightArr[rIdx]
+            let resNum = res === 0 ? Math.abs(res) : res
+            result.push(resNum)
+        }
+        return result
+    }
+}
